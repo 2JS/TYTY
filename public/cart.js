0eb466a5458244e09c165e50b7444759168c7da7
2JS <lego3410@gmail.com>
2018-05-15 14:51:33 +0900

    Sticky Button on cart.js fixed

--- public/cart.js
+++ public/cart.js
@@ -1,10 +1,10 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+var selected = document.getElementById('selected')
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset >= selected.offsetTop + selected.offsetHeight) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
